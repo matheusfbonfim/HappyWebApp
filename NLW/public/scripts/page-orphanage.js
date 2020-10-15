f823d9ccb2586fba6a16e6e135f782b80dfb24ed
Matheus de Farias <matheusbonfim@alunos.utfpr.edu.br>
2020-10-15 10:16:08 -0300

    Update Day#02

--- NLW/public/scripts/page-orphanage.js
+++ NLW/public/scripts/page-orphanage.js
@@ -38,13 +38,46 @@ L
 
 /* Image Gallery - Criação da funcao que recebe o click */
 function selectImage(event){
+    /* currentTarget -> evento atual que está disparando */
     const button = event.currentTarget; /* Retorna exatamente o botão clicado - HTML*/
 
-    /* Percorrer um botao de cada vez e remover a classe active do botão*/
-    /* No caso é só uma */
+    // REMOVER TODAS AS CLASSES .ACTIVE
+    /* Percorrer um botao de cada vez e remover todas classes active do botão
+     No caso é só uma 
+     No documento voce fara uma busca por todos(querySelectorAll)
+     Procura igual no css -> Foi colocado images button pois os botões estão na 
+     classe image */
+    const buttons = document.querySelectorAll(".images button") // Coloca todos os botões em um NOdelist
+    /*console.log(buttons) -> Possivel ver no console do Google que é criado um array dos botões*/ 
+    
+    // Função que será usado em cada botão
+    function removeActiveClass(button){
+        /**No console do Google foi possivel ver que os botões estão não em uma lista
+         * e sim numa NodeList, que tem algumas funcionalidades diferentes
+         * 
+         * Cada botão tem uma classList/ classList é um objeto
+         */
+        button.classList.remove("active")// Remove todas class active dos botões
+    }
+
+    // Percorre cada botao
+    buttons.forEach(removeActiveClass)
     
 
-    /* Selecionar a imagem clicada */
+    // SELECIONAR A IMAGEM CLICADA
+    /* Pegar os filhos do botão que no caso seria a imagem
+       Caso queira ver os filhos, basta colocar console.log(button.children)
+       e ver no console do Google */
+    const image = button.children[0] // Pega a imagem clicada clicada
+    const imageContainer = document.querySelector(".orphanage-details > img") // Pegar somente a imagem maior
 
-    /* Atualizar o container de imagem */
+    /* ATUALIZAR O CONTAINER DE IMAGEM */
+    /* Tendo o imageContainer, é preciso atualizar o container de imagem
+       Quando fizemos o imageContainer, é possivel ter a funcionalidade de src, onde é 
+       possivel atualizar o diretorio */
+    imageContainer.src = image.src 
+
+    /* Imagem maior */ 
+    // ADICIONAR A CLASSE .ACTIVE PARA O BOTÃO SELECIONADO (button)
+    button.classList.add('active') // Adiciona uma class List ao botão clicado
 }
